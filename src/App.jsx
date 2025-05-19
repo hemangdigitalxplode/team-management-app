a44629e1bca459aae7e12cb37910ad341993dfd2
hemangdigitalxplode <hemang@digitalxplode.com>
2025-05-19 14:37:56 +0530

    build error agian changed

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import './index.css'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Tasks from './Pages/Tasks'
+// import AddTask from './Pages/AddTask'
 import AddTask from './Pages/AddTask'
 import Attendance from './Pages/Attendance'
 import OfficeEvents from './Pages/OfficeEvents'
